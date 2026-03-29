51d83bbd4f618b940ec409afc77a67c8f9267a7b
Jules <christdalmeida11@gmail.com>
2026-03-29 18:35:42 +0200

    implement more properly intercept

--- ml_mini/linear_model.py
+++ ml_mini/linear_model.py
@@ -3,7 +3,6 @@ import numpy as np
 class LinearRegression:
    def __init__(self):
          self.coef_ = None
-         self.intercept_ = None
          self.loss_history_ = []
          self.n_iterations_ = None
          self.gradient_method = None
@@ -11,31 +10,35 @@ class LinearRegression:
         "Initialize coefficients and intercept with random values"
         np.random.seed(42)  # For reproducibility
         self.coef_ = np.random.rand(n_features)
-        self.intercept_ = np.random.rand(1)
+
+   def X_bias(self, X):
+        "Add a bias term (intercept) to the input features"
+        n_samples = X.shape[0]
+        bias = np.ones((n_samples, 1))
+        return np.hstack((bias, X))
 
    def predict(self, X):
         "Predict linear regression output given input X"
-        return X @ self.coef_ + self.intercept_
+
+        return X @ self.coef_
    
-   def _batch_gradient_descent(self, X, y, learning_rate, max_iter, eps=1e-6, _start_coef=None, _start_intercept=None):
+   def _batch_gradient_descent(self, X, y, learning_rate, max_iter, eps=1e-6, _start_coef=None):
       """Batch Gradient Descent reduces the loss on the entire dataset"""
 
+      X = self.X_bias(X)
       n_features = X.shape[1]
 
-      if _start_coef is not None and _start_intercept is not None:
+      if _start_coef is not None:
          self.coef_ = _start_coef
-         self.intercept_ = _start_intercept
       else:
          self._generate_coef(n_features)
 
       for i in range(max_iter):
          y_pred = self.predict(X)
 
-         loss_gradient_coef = X.T @ (y_pred - y)
-         loss_gradient_intercept = np.sum(y_pred - y) # (y_pred - y).T @ np.ones(X.shape[0])
+         loss_gradient = X.T @ (y_pred - y)
 
-         self.coef_ -= learning_rate * loss_gradient_coef
-         self.intercept_ -= learning_rate * loss_gradient_intercept
+         self.coef_ -= learning_rate * loss_gradient
 
          self.loss_history_.append(np.mean((y_pred - y) ** 2))
 
@@ -46,22 +49,20 @@ class LinearRegression:
    def _stochastic_gradient_descent(self, X, y, learning_rate, _start_coef=None, _start_intercept=None):
       """Stochastic Gradient Descent updates the coefficients for each training example"""
 
-      n_samples, n_features = X.shape
+      X = self.X_bias(X)
+      n_features = X.shape[1]
 
       if _start_coef is not None and _start_intercept is not None:
          self.coef_ = _start_coef
-         self.intercept_ = _start_intercept
       else:
          n_features = X.shape[1]
          self._generate_coef(n_features)
     
       for x, y in zip(X, y):
          y_pred = self.predict(x)
-         loss_gradient_coef = (y_pred - y) * x
-         loss_gradient_intercept = (y_pred - y)
+         loss_gradient = (y_pred - y) * x
 
-         self.coef_ -= learning_rate * loss_gradient_coef
-         self.intercept_ -= learning_rate * loss_gradient_intercept
+         self.coef_ -= learning_rate * loss_gradient
    
    def fit(self, X, y, learning_rate=0.01, n_iterations=1000, eps=1e-6, method='batch'):
       if method == 'batch':
@@ -78,9 +79,9 @@ class LinearRegression:
    def refit(self, X, y, learning_rate=0.01):
       "Refit the model with the new data"
       if self.gradient_method == 'batch':
-         self._batch_gradient_descent(X, y, learning_rate, max_iter=1000, _start_coef=self.coef_, _start_intercept=self.intercept_)
+         self._batch_gradient_descent(X, y, learning_rate, max_iter=1000, _start_coef=self.coef_)
 
       elif self.gradient_method == 'stochastic':
-         self._stochastic_gradient_descent(X, y, learning_rate, _start_coef=self.coef_, _start_intercept=self.intercept_)
+         self._stochastic_gradient_descent(X, y, learning_rate, _start_coef=self.coef_)
       else:
          raise ValueError("Model has not been fitted yet. Please call fit() before refit().")
